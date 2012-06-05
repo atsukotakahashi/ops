NixOS/nixops

commit 86bbc9a508c4a7c8d4ab7426b08d412c81e525e1

Automatically set up encrypted links between machines
A Charon machine can set the configuration option
‘deployment.encryptedLinksTo’ to specify a set of machines to which an
encrypted peer-to-peer link should be created automatically.  This
generalizes the hacky support for inter-region tunnels in the EC2
backend.  For instance, if machine "proxy" has

   deployment.encryptedLinksTo = [ "backend1" "backend2" ];

then a VPN link over SSH will be automatically set up between "proxy"
and "backend1", and "proxy" and "backend2".  /etc/hosts is set up so a
machine name refers to the encrypted link if available, or the
unencrypted link otherwise; "<NAME>-unencrypted" and
"<NAME>-encrypted" refer to the unencrypted and encrypted links,
respectively.

Fixes #33.

charon/deployment.py
@@ -135,25 +135,72 @@ class Deployment:
     def get_physical_spec(self):
         """Compute the contents of the Nix expression specifying the computed physical deployment attributes"""
 
-        def for_machine(m):
-            lines = []
-            lines.append("  " + m.name + " = { config, pkgs, ... }: {")
+        lines_per_machine = {m.name: [] for m in self.active.itervalues()}
+        authorized_keys = {m.name: [] for m in self.active.itervalues()}
+        kernel_modules = {m.name: set() for m in self.active.itervalues()}
+        hosts = {}
+
+        for m in self.active.itervalues():
+            hosts[m.name] = {}
+            for m2 in self.active.itervalues():
+                if m == m2: continue
+                ip = m.address_to(m2)
+                if ip: hosts[m.name][m2.name] = hosts[m.name][m2.name + "-unencrypted"] = ip
+
+        def do_machine(m):
+            defn = self.definitions[m.name]
+            lines = lines_per_machine[m.name]
+            
             lines.extend(m.get_physical_spec(self.active))
+
+            # Emit configuration to realise encrypted peer-to-peer links.
+            for m2_name in defn.encrypted_links_to:
+                
+                if m2_name not in self.active:
+                    raise Exception("‘deployment.encryptedLinksTo’ in machine ‘{0}’ refers to an unknown machine ‘{1}’"
+                                    .format(m.name, m2_name))
+                m2 = self.active[m2_name]
+                # Don't create two tunnels between a pair of machines.
+                if m.name in self.definitions[m2.name].encrypted_links_to and m.name >= m2.name:
+                    continue
+                local_ipv4 = "192.168.105.{0}".format(m.index)
+                remote_ipv4 = "192.168.105.{0}".format(m2.index)
+                lines.append('    networking.p2pTunnels.{0} ='.format(m2.name))
+                lines.append('      {{ target = "{0}-unencrypted";'.format(m2.name))
+                lines.append('        localTunnel = {0};'.format(10000 + m2.index))
+                lines.append('        remoteTunnel = {0};'.format(10000 + m.index))
+                lines.append('        localIPv4 = "{0}";'.format(local_ipv4))
+                lines.append('        remoteIPv4 = "{0}";'.format(remote_ipv4))
+                lines.append('        privateKey = "/root/.ssh/id_charon_vpn";')
+                lines.append('      }};'.format(m2.name))
+                # FIXME: set up the authorized_key file such that ‘m’
+                # can do nothing more than create a tunnel.
+                authorized_keys[m2.name].append('"' + m._public_vpn_key + '"')
+                kernel_modules[m.name].add('"tun"')
+                kernel_modules[m2.name].add('"tun"')
+                hosts[m.name][m2.name] = hosts[m.name][m2.name + "-encrypted"] = remote_ipv4
+                hosts[m2.name][m.name] = hosts[m2.name][m.name + "-encrypted"] = local_ipv4
+            
             private_ipv4 = m.private_ipv4
             if private_ipv4: lines.append('    networking.privateIPv4 = "{0}";'.format(private_ipv4))
             public_ipv4 = m.public_ipv4
             if public_ipv4: lines.append('    networking.publicIPv4 = "{0}";'.format(public_ipv4))
-            hosts = []
-            for m2 in self.active.itervalues():
-                if m == m2: continue
-                ip = m.address_to(m2)
-                if ip: hosts.append("{0} {1}".format(ip, m2.name))
-            lines.append('    networking.extraHosts = "{0}\\n";'.format('\\n'.join(hosts)))
-            lines.append('    boot.kernelModules = [ "tun" ];')
+            
+        for m in self.active.itervalues(): do_machine(m)
+
+        def emit_machine(m):
+            lines = []
+            lines.append("  " + m.name + " = { config, pkgs, ... }: {")
+            lines.extend(lines_per_machine[m.name])
+            if authorized_keys[m.name]:
+                lines.append('    users.extraUsers.root.openssh.authorizedKeys.keys = [ {0} ];'.format(" ".join(authorized_keys[m.name])))
+                lines.append('    services.openssh.extraConfig = "PermitTunnel yes\\n";')
+            lines.append('    boot.kernelModules = [ {0} ];'.format(" ".join(kernel_modules[m.name])))
+            lines.append('    networking.extraHosts = "{0}\\n";'.format('\\n'.join([hosts[m.name][m2] + " " + m2 for m2 in hosts[m.name]])))
             lines.append("  };\n")
             return "\n".join(lines)
 
-        return "".join(["{\n"] + [for_machine(m) for m in self.active.itervalues()] + ["}\n"])
+        return "".join(["{\n"] + [emit_machine(m) for m in self.active.itervalues()] + ["}\n"])
             
 
     def build_configs(self, include, exclude, dry_run=False):
@@ -266,9 +313,8 @@ class Deployment:
                 if not should_do(m, include, exclude): return
                 m.create(self.definitions[m.name], check=check)
                 m.wait_for_ssh(check=check)
+                m.generate_vpn_key()
             charon.parallel.run_tasks(nr_workers=len(self.active), tasks=self.active.itervalues(), worker_fun=worker)
-
-        if create_only: return
 
         # Build the machine configurations.
         if dry_run:

charon/backends/__init__.py
@@ -19,6 +19,7 @@ class MachineDefinition:
     def __init__(self, xml):
         self.name = xml.get("name")
         assert self.name
+        self.encrypted_links_to = set([e.get("value") for e in xml.findall("attrs/attr[@name='encryptedLinksTo']/list/string")])
 
 
 class MachineState:
@@ -36,6 +37,7 @@ class MachineState:
         self._ssh_pinged_this_time = False
         self._ssh_master_started = False
         self._ssh_master_opts = []
+        self._public_vpn_key = None
         self.index = None
 
         # Nix store path of the last global configuration deployed to
@@ -63,6 +65,7 @@ class MachineState:
         if self.cur_configs_path: x['vmsPath'] = self.cur_configs_path
         if self.cur_toplevel: x['toplevel'] = self.cur_toplevel
         if self._ssh_pinged: x['sshPinged'] = self._ssh_pinged
+        if self._public_vpn_key: x['publicVpnKey'] = self._public_vpn_key
         if self.index != None: x['index'] = self.index
         return x
 
@@ -71,7 +74,8 @@ class MachineState:
         self.cur_configs_path = x.get('vmsPath', None)
         self.cur_toplevel = x.get('toplevel', None)
         self._ssh_pinged = x.get('sshPinged', False)
-        self._index = x.get('index', None)
+        self._public_vpn_key = x.get('publicVpnKey', None)
+        self.index = x.get('index', None)
 
     def destroy(self):
         """Destroy this machine, if possible."""
@@ -166,10 +170,10 @@ class MachineState:
                 raise Exception("command ‘{0}’ failed on machine ‘{1}’".format(command, self.name))
             return res
 
-    def _create_key_pair(self):
+    def _create_key_pair(self, key_name="Charon auto-generated key"):
         key_dir = self.depl.tempdir + "/ssh-key-" + self.name
         os.mkdir(key_dir, 0700)
-        res = subprocess.call(["ssh-keygen", "-t", "dsa", "-f", key_dir + "/key", "-N", '', "-C", "Charon auto-generated key"],
+        res = subprocess.call(["ssh-keygen", "-t", "dsa", "-f", key_dir + "/key", "-N", '', "-C", key_name],
                               stdout=charon.util.devnull)
         if res != 0: raise Exception("unable to generate an SSH key")
         f = open(key_dir + "/key"); private = f.read(); f.close()
@@ -190,6 +194,23 @@ class MachineState:
             env=env).wait()
         if res != 0:
             raise Exception("unable to copy closure to machine ‘{0}’".format(self.name))
+
+    def generate_vpn_key(self):
+        if self._public_vpn_key: return
+        (private, public) = self._create_key_pair(key_name="Charon VPN key of {0}".format(self.name))
+        f = open(self.depl.tempdir + "/id_vpn-" + self.name, "w+")
+        f.write(private)
+        f.seek(0)
+        # FIXME: use run_command
+        res = subprocess.call(
+            ["ssh", "-x", "root@" + self.get_ssh_name()]
+            + self.get_ssh_flags() +
+            ["umask 077 && mkdir -p /root/.ssh && cat > /root/.ssh/id_charon_vpn"],
+            stdin=f)
+        f.close()
+        if res != 0: raise Exception("unable to upload VPN key to ‘{0}’".format(self.name))
+        self._public_vpn_key = public
+        self.write()
 
 
 import charon.backends.none

charon/backends/ec2.py
@@ -79,7 +79,6 @@ class EC2State(MachineState):
         self._tags = {}
         self._block_device_mapping = {}
         self._public_host_key = False
-        self._public_vpn_key = False
         self._root_device_type = None
         
         
@@ -103,7 +102,6 @@ class EC2State(MachineState):
         if self._tags: y['tags'] = self._tags
         if self._block_device_mapping: y['blockDeviceMapping'] = self._block_device_mapping
         if self._public_host_key: y['publicHostKey'] = self._public_host_key
-        if self._public_vpn_key: y['publicVpnKey'] = self._public_vpn_key
         if self._root_device_type: y['rootDeviceType'] = self._root_device_type
         if self._elastic_ipv4: y['elasticIPv4'] = self._elastic_ipv4
         x['ec2'] = y
@@ -131,8 +129,6 @@ class EC2State(MachineState):
         self._tags = y.get('tags', {})
         self._block_device_mapping = y.get('blockDeviceMapping', {})
         self._public_host_key = y.get('publicHostKey', None)
-        self._vpn_key_set = y.get('vpnKeySet', False)
-        self._public_vpn_key = y.get('publicVpnKey', None)
         self._root_device_type = y.get('rootDeviceType', None)
         self._elastic_ipv4 = y.get('elasticIPv4', None)
 
@@ -148,35 +144,7 @@ class EC2State(MachineState):
 
     
     def get_physical_spec(self, machines):
-        lines = ['    require = [ <nixos/modules/virtualisation/amazon-config.nix> ];',
-                 '    services.openssh.extraConfig = "PermitTunnel yes\\n";']
-        authorized_keys = []
-        tun = 0
-        for m in machines.itervalues():
-            tun = tun + 1
-            if self != m and isinstance(m, EC2State) and self._region != m._region:
-                # The two machines are in different regions, so they
-                # can't talk directly to each other over their private
-                # IP.  So create a VPN connection over their public
-                # IPs to forward the private IPs.
-                if self.name > m.name:
-                    # Since it's a two-way tunnel, we only need to
-                    # start it on one machine (for each pair of
-                    # machines).  Pick the one that has the higher
-                    # name (lexicographically).
-                    lines.append('    jobs."vpn-to-{0}" = {{'.format(m.name))
-                    lines.append('      startOn = "started network-interfaces";')
-                    lines.append('      path = [ pkgs.nettools pkgs.openssh ];')
-                    lines.append('      daemonType = "fork";')
-                    lines.append('      exec = "ssh -i /root/.ssh/id_vpn -o StrictHostKeyChecking=no -f -x -w {0}:{0} {4} \'ifconfig tun{0} {2} {3} netmask 255.255.255.255; route add {2}/32 dev tun{0}\'";'
-                                 .format(tun, m.name, self._private_ipv4, m._private_ipv4, m._public_ipv4))
-                    lines.append('      postStart = "ifconfig tun{0} {2} {1} netmask 255.255.255.255; route add {2}/32 dev tun{0}";'
-                                 .format(tun, self._private_ipv4, m._private_ipv4))
-                    lines.append('    };')
-                else:
-                    # The other side just needs an authorized_keys entry.
-                    authorized_keys.append('"' + m._public_vpn_key + '"')
-        lines.append('    users.extraUsers.root.openssh.authorizedKeys.keys = [ {0} ];'.format(" ".join(authorized_keys)))
+        lines = ['    require = [ <nixos/modules/virtualisation/amazon-config.nix> ];']
 
         for k, v in self._block_device_mapping.items():
             if v.get('encrypt', False) and v.get('passphrase', "") == "" and v.get('generatedKey', "") != "":
@@ -524,24 +492,6 @@ class EC2State(MachineState):
             if v.get('encrypt', False) and v.get('passphrase', "") == "" and v.get('generatedKey', "") == "":
                 v['generatedKey'] = charon.util.generate_random_string(length=256)
                 self.write()
-
-        # Generate an SSH key for ad hoc VPN links between EC2
-        # machines, and upload the private half.
-        if not self._public_vpn_key:
-            (private, public) = self._create_key_pair()
-            f = open(self.depl.tempdir + "/id_vpn-" + self.name, "w+")
-            f.write(private)
-            f.seek(0)
-            # FIXME: use run_command
-            res = subprocess.call(
-                ["ssh", "-x", "root@" + self.get_ssh_name()]
-                + self.get_ssh_flags() +
-                ["umask 077 && mkdir -p /root/.ssh && cat > /root/.ssh/id_vpn"],
-                stdin=f)
-            f.close()
-            if res != 0: raise Exception("unable to upload VPN key to ‘{0}’".format(self.name))
-            self._public_vpn_key = public
-            self.write()
 
 
     def _delete_volume(self, volume_id):

examples/encryption-test.nix
@@ -1,0 +1,25 @@
+{
+  network.description = "Encryption test";
+
+  machine1 =
+    { config, pkgs, ... }:
+    { deployment.targetEnv = "virtualbox";
+      deployment.encryptedLinksTo = [ "machine2" "machine3" ];
+    };
+
+  machine2 =
+    { config, pkgs, ... }:
+    { deployment.targetEnv = "virtualbox";
+      deployment.encryptedLinksTo = [ "machine1" "machine3" ];
+      
+      services.httpd.enable = true;
+      services.httpd.adminAddr = "e.dolstra@tudelft.nl";
+      services.httpd.documentRoot = "/tmp";
+    };
+
+  machine3 =
+    { config, pkgs, ... }:
+    { deployment.targetEnv = "virtualbox";
+      deployment.encryptedLinksTo = [ "machine1" "machine2" ];
+    };
+}

nix/eval-machine-info.nix
@@ -44,7 +44,7 @@ rec {
   
     machines =
       flip mapAttrs nodes (n: v:
-        { inherit (v.config.deployment) targetEnv targetHost;
+        { inherit (v.config.deployment) targetEnv targetHost encryptedLinksTo;
           adhoc = optionalAttrs (v.config.deployment.targetEnv == "adhoc") v.config.deployment.adhoc;
           ec2 = optionalAttrs (v.config.deployment.targetEnv == "ec2") v.config.deployment.ec2;
           virtualbox =

nix/options.nix
@@ -10,7 +10,7 @@ in
 
 {
 
-  imports = [ ./ec2.nix ];
+  imports = [ ./ec2.nix ./ssh-tunnel.nix ];
   
   
   options = {
@@ -45,6 +45,21 @@ in
       description = ''
         This option specifies the hostname or IP address to be used by
         Charon to execute remote deployment operations.
+      '';
+    };
+
+    deployment.encryptedLinksTo = mkOption {
+      default = [];
+      type = types.list types.string;
+      description = ''
+        Charon will set up an encrypted tunnel (via SSH) to the
+        machines listed here.  Since this is a two-way (peer to peer)
+        connection, it is not necessary to set this option on both
+        endpoints.  Charon will set up <filename>/etc/hosts</filename>
+        so that the host names of the machines listed here resolve to
+        the IP addresses of the tunnels.  It will also add the alias
+        <literal><replaceable>machine</replaceable>-encrypted</literal>
+        for each machine.
       '';
     };
 

nix/ssh-tunnel.nix
@@ -1,0 +1,78 @@
+{ config, pkgs, ... }:
+
+with pkgs.lib;
+
+{
+
+  ###### interface
+
+  options = {
+
+    networking.p2pTunnels = mkOption {
+      default = { };
+      example =
+        { tunnel1 =
+            { target = "192.0.2.1";
+              privateKey = "/root/.ssh/id_vpn";
+              localTunnel = 0;
+              remoteTunnel = 1;
+              localIPv4 = "172.16.12.1";
+              remoteIPv4 = "172.16.12.2";
+            };
+        };
+      type = types.attrsOf types.optionSet;
+      options = {
+        target = mkOption {
+          type = types.uniq types.string;
+          description = "Host name or IP address of the remote machine.";
+        };
+        privateKey = mkOption {
+          type = types.uniq types.path;
+          description = "Path to the private key file used to connect to the remote machine.";
+        };
+        localTunnel = mkOption {
+          type = types.uniq types.int;
+          description = "Local tunnel device number.";
+        };
+        remoteTunnel = mkOption {
+          type = types.uniq types.int;
+          description = "Remote tunnel device number.";
+        };
+        localIPv4 = mkOption {
+          type = types.uniq types.string;
+          description = "IPv4 address of the local endpoint of the tunnel.";
+        };
+        remoteIPv4 = mkOption {
+          type = types.uniq types.string;
+          description = "IPv4 address of the remote endpoint of the tunnel.";
+        };
+      };
+      description = ''
+        A set of peer-to-peer tunnels set up automatically over SSH.
+      '';
+    };
+
+  };
+  
+
+  ###### implementation
+
+  config = {
+
+    jobs = flip mapAttrs' config.networking.p2pTunnels (n: v: nameValuePair "ssh-tunnel-${n}" {
+      startOn = "starting networking and started network-interfaces and started udev";
+      stopOn = "stopping network-interfaces";
+      path = [ pkgs.nettools pkgs.openssh ];
+      preStart = "sleep 1"; # FIXME: hack to work around Upstart
+      # FIXME: ensure that the remote tunnel device is free
+      exec =
+        "ssh -i ${v.privateKey} -x"
+        + " -o StrictHostKeyChecking=no -o PermitLocalCommand=yes"
+        + " -o LocalCommand='ifconfig tun${toString v.localTunnel} ${v.localIPv4} pointopoint ${v.remoteIPv4} netmask 255.255.255.255; route add ${v.remoteIPv4}/32 dev tun${toString v.localTunnel}'"
+        + " -w ${toString v.localTunnel}:${toString v.remoteTunnel} ${v.target}"
+        + " 'ifconfig tun${toString v.remoteTunnel} ${v.remoteIPv4} pointopoint ${v.localIPv4} netmask 255.255.255.255; route add ${v.localIPv4}/32 dev tun${toString v.remoteTunnel}'";
+    });
+
+  };
+  
+}

atsukotakahashi/ops

commit 112672e854d27434fbffe2fa600f8fa4b23f91d6

Fix "charon deploy --create-only"
This flag was accidentally broken in 86bbc9a508c4a7c8d4ab7426b08d412c81e525e1.

charon/deployment.py
@@ -316,6 +316,8 @@ class Deployment:
                 m.generate_vpn_key()
             charon.parallel.run_tasks(nr_workers=len(self.active), tasks=self.active.itervalues(), worker_fun=worker)
 
+        if create_only: return
+        
         # Build the machine configurations.
         if dry_run:
             self.build_configs(dry_run=True, include=include, exclude=exclude)